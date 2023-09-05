36041151665d7c6efee7a8dd9f6387bca9ab051b
Otávio/Wumq <128698772+Wumq157@users.noreply.github.com>
2023-09-05 11:53:02 -0300

    Create aula1.js

---
+++ aula1.js
@@ -0,0 +1,18 @@
+
+
+//var num1 = Number(2);
+//var num2 = Number(7);
+
+var soma
+
+function calcularSoma(num1, num2){
+//var numeros = String(num1) + " + " +  String(num2);
+soma = num1 + num2;
+return soma;
+
+}
+console.log(`Se somar o valor ${num1} com ${num2} teremos: O resultado é:`, calcularSoma(45, 232));
+
+//console.log(resultado)
+//console.log(numeros)
+//console.log(soma);
