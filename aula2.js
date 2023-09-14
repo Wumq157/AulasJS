293da374c9f63c15cc048b20604e42412f59edc9
Otávio/Wumq <128698772+Wumq157@users.noreply.github.com>
2023-09-14 19:34:04 -0300

    Update aula2.js

--- aula2.js
+++ aula2.js
@@ -1,5 +1,20 @@
-function text(){
-    console.log("Hello World.");
-    };
-    
-    text()
+// Atividade 1
+function enviarMensagem(mensagem) {
+    console.log("A mensagem enviada foi: " + mensagem);
+}
+
+// Atividade 2
+function calcularCubo(numeroFornecido) {
+    return numeroFornecido * numeroFornecido * numeroFornecido;
+}
+
+// Atividade 3
+function calculeTrianguloArea(base, altura) {
+    return (base * altura) / 2;
+}
+// Execução da Atividade 1
+enviarMensagem("Olá Mundo!!!")
+// Execução da Atividade 2
+console.log(`O valor do cubo é de: ${calcularCubo(3)}`)
+// Execução da Atividade 3
+console.log(`A área do triangulo é de: ${calculeTrianguloArea(2, 5)}`)
