ff8af6526e78c5c21604df2a50c0838e77112317
Otávio/Wumq <128698772+Wumq157@users.noreply.github.com>
2024-03-01 11:51:04 -0300

    Create variaveis e operadores.js

---
+++ enron/variaveis e operadores.js
@@ -0,0 +1,36 @@
+//variaveis primitivas
+//string
+let nome = 'otavio';
+//inteiro
+let idade = 17;
+//flutuannte
+let altura = 1.80;
+//boleana
+let casado = false
+
+console.log(nome, idade, altura, casado);
+// não primitivo
+//obejeto pessoa
+pessoa = {
+    nome: 'nome',
+    idade: 17,
+    altura: 1.80,
+    casado: true
+}
+//array
+let array = [12, 23, 45, 56, 76, 78, 89, 90]
+
+//operadores
+let num1 = 1
+let num2 = 1
+
+//soma
+let soma = num1 + num2
+//subtração 
+let sub = num1 - num2
+//multiplicação
+let mult = num1 * nmu2
+//divisão
+let div = num1 / num2 
+//resto
+let rest = num1 % num2
